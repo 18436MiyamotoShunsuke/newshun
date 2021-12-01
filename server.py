a6239f79ce62f58bf2088087a2ab4674d71fb681
Shunsuke Miyamoto <18436@toba-cmt.ac.jp>
2021-12-01 15:21:35 +0900

    コードを追加した

---
+++ server.py
@@ -0,0 +1,18 @@
+import zmq
+def start_server():
+    context = zmq.Context()
+    socket =context.socket(zmq.REP)
+    socket.bind("tcp://*:5556")
+
+    print("Server startup.")
+
+    while True:
+        message = socket.recv_string()
+        print("Received message = %s"%message)
+        socket.send_string("Reply:%s"%message)
+
+    socket.close()
+    context.destroy()
+
+if __name__ == "__main__":
+    start_server()
